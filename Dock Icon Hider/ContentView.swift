74f151fab70252bc0e9b74247b16546e8d3584e4
johnbean393 <johnbean393@gmail.com>
2024-05-22 14:16:21 +0800

    Initial Commit

---
+++ Dock Icon Hider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dock Icon Hider
+//
+//  Created by Bean John on 22/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dock Icon Hider/Dock_Icon_HiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dock_Icon_HiderApp.swift
+//  Dock Icon Hider
+//
+//  Created by Bean John on 22/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dock_Icon_HiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
